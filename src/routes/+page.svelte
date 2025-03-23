ba7c98072bf09a64a5dc1643070925e89f4d9bfd
Naawa <naous.islam@outlook.com>
2025-03-22 23:40:56 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+	import Activity from './components/Activity.svelte';
+	import Clock from './components/Clock.svelte';
+	import {
+		activities,
+		firstHourTimeBlocks,
+		secondHourTimeBlocks,
+		thirdHourTimeBlocks,
+		lastHourTimeBlocks,
+		seniorFirstHourTimeBlocks,
+		seniorSecondHourTimeBlocks,
+		seniorThirdHourTimeBlocks,
+		seniorLastHourTimeBlocks,
+		seniorActivities
+	} from './components/data';
+
+	let now: Date = $state(new Date());
+	let startTime: Date = firstHourTimeBlocks[0];
+	let endTime: Date = lastHourTimeBlocks[lastHourTimeBlocks.length - 1];
+	endTime.setHours(lastHourTimeBlocks[0].getHours() + 1, lastHourTimeBlocks[0].getMinutes(), 0, 0);
+
+	let seniorEndTime: Date = seniorLastHourTimeBlocks[seniorLastHourTimeBlocks.length - 1];
+	seniorEndTime.setHours(
+		seniorLastHourTimeBlocks[0].getHours() + 1,
+		seniorLastHourTimeBlocks[0].getMinutes(),
+		0,
+		0
+	);
+
+	let timeBlocks: Date[] = $state(lastHourTimeBlocks);
+	let seniorTimeblocks: Date[] = $state(seniorLastHourTimeBlocks);
+
+	function setHour(): Date[] {
+		let timeblocks: Date[];
+
+		if (now.getTime() < secondHourTimeBlocks[0].getTime()) {
+			timeblocks = firstHourTimeBlocks;
+		} else if (now.getTime() < thirdHourTimeBlocks[0].getTime()) {
+			timeblocks = secondHourTimeBlocks;
+		} else if (now.getTime() < lastHourTimeBlocks[0].getTime()) {
+			timeblocks = thirdHourTimeBlocks;
+		} else if (now.getTime() < endTime.getTime()) {
+			timeblocks = lastHourTimeBlocks;
+		} else {
+			timeblocks = firstHourTimeBlocks;
+		}
+		return timeblocks;
+	}
+
+	function setSeniorHour(): Date[] {
+		let seniorTimeblocks: Date[];
+
+		if (now.getTime() < seniorSecondHourTimeBlocks[0].getTime()) {
+			seniorTimeblocks = seniorFirstHourTimeBlocks;
+		} else if (now.getTime() < seniorThirdHourTimeBlocks[0].getTime()) {
+			seniorTimeblocks = seniorSecondHourTimeBlocks;
+		} else if (now.getTime() < seniorLastHourTimeBlocks[0].getTime()) {
+			seniorTimeblocks = seniorThirdHourTimeBlocks;
+		} else if (now.getTime() < seniorEndTime.getTime()) {
+			seniorTimeblocks = seniorLastHourTimeBlocks;
+		} else {
+			seniorTimeblocks = seniorFirstHourTimeBlocks;
+		}
+		return seniorTimeblocks;
+	}
+
+	function hourStart(now: Date, startTime: Date, endTime: Date): boolean {
+		if (now > startTime && now < endTime) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function updateTime(): void {
+		now = new Date();
+		timeBlocks = setHour();
+		seniorTimeblocks = setSeniorHour();
+		setTimeout(updateTime, 1);
+	}
+	updateTime();
+</script>
+
+<Clock
+	time={now.toLocaleTimeString('en-US', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit'
+	})}
+></Clock>
+<section>
+	<div>
+		<h1>Junior Activites</h1>
+		<br />
+		<span class="jr-divider"></span>
+		{#each activities as activity, i}
+			<Activity
+				{now}
+				name={activity.name}
+				start={timeBlocks[i]}
+				end={timeBlocks[i + 1]}
+				image={activity.image}
+				junior={true}
+			></Activity>
+			<span class="jr-divider"></span>
+		{/each}
+		<br />
+	</div>
+	<div>
+		<h1>Create Activites</h1>
+		<br />
+		<span></span>
+		{#each seniorActivities as activity, i}
+			<Activity
+				{now}
+				name={activity.name}
+				start={seniorTimeblocks[i]}
+				end={seniorTimeblocks[i + 1]}
+				image={activity.image}
+				junior={false}
+			></Activity>
+			<span></span>
+		{/each}
+		<br />
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: stretch;
+		padding: 0;
+		width: 100svw;
+		gap: 4em;
+		max-height: calc(100svh - 12em);
+		position: relative;
+	}
+
+	div {
+		width: fit-content;
+		background-color: #fff4ff;
+		border-radius: 0.75em;
+		color: #a839b9;
+		border: solid 0.3em #a839b9;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 2em;
+		flex-direction: column;
+	}
+	div:last-of-type {
+		background-color: #f4f7ff;
+		border: solid 0.3em #1376bd;
+		color: #1376bd;
+	}
+
+	span {
+		width: 95%;
+		height: 0.2em;
+		background-color: #1376bd20;
+		border-radius: 1em;
+	}
+
+	.jr-divider {
+		width: 95%;
+		height: 0.2em;
+		background-color: #a839b920;
+		border-radius: 1em;
+	}
+</style>
---
+++ src/routes/components/data.ts
@@ -0,0 +1,100 @@
+interface Activity {
+    name: string,
+    image: string,
+}
+
+function getTimeBlocks(weekday: boolean, hour: number) {
+    let timeBlocks: Date[] = []
+    let minute: number = 0
+    if (weekday) {
+        minute = 30
+    }
+    for (let i = 0; i < 6; i++) {
+        if (minute == 60) {
+            minute = 0
+            hour += 1;
+        }
+        let activityTimeBlock = new Date()
+        activityTimeBlock.setHours(hour, minute, 0, 0)
+        timeBlocks.push(activityTimeBlock)
+        if (i == 4) {
+            minute += 20
+        }
+        else {
+            minute += 10
+        }
+    }
+    return timeBlocks
+}
+
+function getSeniorTimeBlocks(weekday: boolean, hour: number, numberOfActivities: number) {
+    let timeBlocks: Date[] = []
+    let minute: number = 0
+    if (weekday) {
+        minute = 30
+    }
+    for (let i = 0; i < numberOfActivities + 1; i++) {
+        if (minute == 60) {
+            minute = 0
+            hour += 1;
+        }
+        let activityTimeBlock = new Date()
+        activityTimeBlock.setHours(hour, minute, 0, 0)
+        timeBlocks.push(activityTimeBlock)
+        if (i == numberOfActivities - 2) {
+            minute += 40
+        }
+        else {
+            minute += 10
+        }
+    }
+    return timeBlocks
+}
+
+export const firstHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 10) : getTimeBlocks(true, 15)
+export const secondHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 11) : getTimeBlocks(true, 16)
+export const thirdHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 12) : getTimeBlocks(true, 17)
+export const lastHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 13) : getTimeBlocks(true, 18)
+
+export const activities: Activity[] = [
+    {
+        name: "Typing",
+        image: "keyboard.svg"
+    },
+    {
+        name: "CodeSpark",
+        image: "code-spark.svg"
+    },
+    {
+        name: "Mouse Games",
+        image: "mouse-games.svg"
+    },
+    {
+        name: "Code.org",
+        image: "code.svg"
+    },
+    {
+        name: "Free Time",
+        image: "free-time.svg"
+    },
+]
+
+export const seniorActivities: Activity[] = [
+    {
+        name: "Typing",
+        image: "keyboard.svg"
+    },
+    {
+        name: "Impact",
+        image: "code-spark.svg"
+    },
+    {
+        name: "Exploration",
+        image: "free-time.svg"
+    },
+]
+
+export const seniorFirstHourTimeBlocks = new Date().getDay() == 6 ? getSeniorTimeBlocks(false, 10, seniorActivities.length) : getSeniorTimeBlocks(true, 15, seniorActivities.length)
+export const seniorSecondHourTimeBlocks = new Date().getDay() == 6 ? getSeniorTimeBlocks(false, 11, seniorActivities.length) : getSeniorTimeBlocks(true, 16, seniorActivities.length)
+export const seniorThirdHourTimeBlocks = new Date().getDay() == 6 ? getSeniorTimeBlocks(false, 12, seniorActivities.length) : getSeniorTimeBlocks(true, 17, seniorActivities.length)
+export const seniorLastHourTimeBlocks = new Date().getDay() == 6 ? getSeniorTimeBlocks(false, 13, seniorActivities.length) : getSeniorTimeBlocks(true, 18, seniorActivities.length)
